dd75c8c4c64176f9d015bbf8ae915e912e215543
cryptogus <wnelem1234@naver.com>
2024-02-16 23:53:27 +0900

    Add garp poc code Add getting network information code

---
+++ src/arphdr.h
@@ -0,0 +1,30 @@
+#ifndef __ARPHDR_H
+#define __ARPHDR_H
+
+#include <stdint.h>
+// HardwareType (HTYPE)
+
+#define	ETHER 1    // Ethernet 10Mbps
+#define	EETHER 2   // Experimental Ethernet 3Mbps
+#define	AX25 3     // Amateur Radio AX.25
+#define	PRONET 4   // PROnet token ring
+#define	CHAOS 5    // Chaosnet
+#define IEEE802 6  // IEEE 802.2 Ethernet/TR/TB
+#define ARCNET 7   // ARCnet
+#define	HC 8       // Hyperchannel
+#define	LANSTAR 9  // Lanstar
+
+typedef struct arphdr_t {
+    uint16_t htype;    // Hardware type (Ethernet)
+    uint16_t ptype;    // Protocol type (IPv4)
+    uint8_t  hlen;     // Hardware address length
+    uint8_t  plen;     // Protocol address length
+    uint16_t oper;     // Operation (ARP Reply)
+    uint8_t  smac[6];  // Sender hardware address (MAC address)
+    uint32_t sip;      // Sender protocol address (IP address)
+    uint8_t  tmac[6];  // Target hardware address (MAC address)
+    uint32_t tip;      // Target protocol address (IP address)
+} ArpHdr_t;
+
+
+#endif
